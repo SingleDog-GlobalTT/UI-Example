d09b66bad4978d195bdd9d14fdd62bb1120aaa79
tyouki <tyouki1993@yahoo.co.jp>
2017-05-16 17:59:14 +0900

    add new interface of login&register

---
+++ www/js/Login_form.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tyouki on 2017/05/15.
+ */
+$('form#login_form').submit(function (event) {
+    event.preventDefault();
+
+    var $login_form = $(this),
+        username = $login_form.find('input[name="username"]').val(),
+        password = $login_form.find('input[name="password"]').val(),
+        url = $login_form.attr('action');
+
+    console.log(gender,username,password,email,tel,year,month,day,image,url);
+
+    $.post(url,{
+        username:username,
+        password:password,
+        url:url
+    },function (return_data) {
+        console.log(return_data);
+    });
+
+});

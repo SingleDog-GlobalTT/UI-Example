99904b61fb4cabacd6343c3db19d2f3cd77ef144
Kiririn00 <Varit@Varits-MacBook-Pro.local>
2017-06-05 19:27:20 +0900

    リング修正（全部じゃないけど） create_answer（create_questionにするべきと思う）のscriptが完成。テスト済み。

--- www/js/create_question.js
+++ www/js/create_question.js
@@ -1,6 +1,19 @@
 /**
  * Created by Varit on 6/5/17.
  */
+
+//get the user_id session
+var user_id = sessionStorage.user_id;
+
+//check user if. user have to login into the system
+if(user_id == 0 || user_id == null){
+    alert("The session is expire, please login again");
+    window.location.replace("/index.html");
+}
+else{
+    console.log("current user_id: ", user_id);
+}
+
 $('form#create_question').submit(function (event) {
 
     event.preventDefault();
@@ -13,8 +26,10 @@ $('form#create_question').submit(function (event) {
     console.log(question, category_id, url);
 
     $.post(url,{
-
+        question_name:question,
+        category_id:category_id,
+        user_id:sessionStorage.user_id
     }, function (status) {
-
+        console.log("status: ", status);
     });
 });

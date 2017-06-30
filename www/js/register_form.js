cf3cda8970c8f05970513f1f3ce67eab9c987eea
Kiririn00 <Varit@Varits-MacBook-Pro.local>
2017-07-01 01:38:04 +0900

    Done Upload Image system (already test)

--- www/js/register_form.js
+++ www/js/register_form.js
@@ -5,35 +5,22 @@
 $('form#register_form').submit(function (event) {
     event.preventDefault();
 
-    var $register_form = $(this),
-        username = $register_form.find('input[name="username"]').val(),
-        gender = $register_form.find('select[name="gender"]').val(),
-        password = $register_form.find('input[name="password"]').val(),
-        email = $register_form.find('input[name="email"]').val(),
-        tel = $register_form.find('input[name="tel"]').val(),
-        year = $register_form.find('select[name="year"]').val(),
-        month = $register_form.find('select[name="month"]').val(),
-        day = $register_form.find('select[name="day"]').val(),
-        image = $register_form.find('input[name="image"]').val(),
-        postcode = $register_form.find('input[name="postcode"]').val(),
-    url = $register_form.attr('action');
+    var params = new FormData( $(this)[0] );
 
-    console.log(gender,username,password,email,tel,year,month,day,image,url);
+    $.ajax({
+        type: "POST",
+        url: $(this).attr('action'),
+        contentType: false,
+        processData: false,
+        data: params,
+        dataType: "json",
+        success: function( json ) {
+           console.log(json);
 
-    $.post(url,{
-        gender:gender,
-        username:username,
-        password:password,
-        email:email,
-        tel:tel,
-        year:year,
-        month:month,
-        day:day,
-        postcode:postcode,
-        image:image,
-        url:url
-    },function (return_data) {
-        console.log(return_data);
+            if(json.status == "Success"){
+               window.location.replace("/index.html");
+            }
+        }
     });
 
 });

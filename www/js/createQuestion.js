bf7a2cb311a2f967e05d3549d50bf47fac2dd877
OuKiWang <wy5100152@gmail.com>
2017-07-03 21:15:54 +0900

    update

--- www/js/createQuestion.js
+++ www/js/createQuestion.js
@@ -7,11 +7,12 @@ $('form#create_form').submit(function (event) {
     event.preventDefault();
 
     var $create_form = $(this);
-    var question_name = $reate_form.find('input[name="question"]').val();
-    var category_id = $reate_form.find('select[name="category_id"]').val();
+    var question_name = $create_form.find('input[name="question"]').val();
+    var category_id = $create_form.find('select[name="category_id"]').val();
     var url = $create_form.attr('action');
 
     console.log(question_name, category_id, url);
+    /*
     $.post(url, 
         {
         question_name: question_name,
@@ -19,6 +20,7 @@ $('form#create_form').submit(function (event) {
         user_id: user_id
         }
         )
+        */
     if (user_id == null || user_id == "undefined") {
         window.location.replace("/index.html");
     }
